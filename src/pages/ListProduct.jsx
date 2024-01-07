d5cc6c39f445f3e410dd339237c2994b2d4b595b
khangph26947 <khangndph26947@fpt.edu.vn>
2024-01-07 21:49:57 +0700

    list product

---
+++ src/pages/ListProduct.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ListProduct = () => {
+  return (
+   <>
+<div className='container mx-auto'>
+    list product
+</div>
+   </>
+  )
+}
+
+export default ListProduct
